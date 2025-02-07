c5ca47ab754c48f10b453e3330fa9285f0fe8a6c
Weerakoon Thisara Rathnayaka <thisara.weerakoon2001@gmail.com>
2025-02-07 14:41:38 +0530

    Added main.go

--- SampleBuilders/main.go
+++ SampleBuilders/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+
 	"github.com/ThisaraWeerakoon/Initial_Synapse_Go/SampleBuilders/pkg/jsonbuilder"
+	"github.com/ThisaraWeerakoon/Initial_Synapse_Go/SampleBuilders/pkg/xmlbuilder"
 )
 
 func main() {
 	jsonbuilder.JSONBuilderRunner()
+	xmlbuilder.XMLBuilderRunner()
 
 }
